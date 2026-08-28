395f649beb87512bcbed6bcd9b705290ebd764dc
Mozzer <GrozzerMozzer@gmail.com>
2026-08-28 20:07:49 -0300

    feat: atividades zipadas para facilidade de acesso(#17)

--- atividades/java/lista_um_24_08/mensagem.java
+++
@@ -1,7 +0,0 @@
-public class mensagem {
-    public static void main(String[] args) {
-        String nome = "João";
-        int idade = 25;
-        System.out.println("Ola, " + nome + "! Você tem " + idade + " anos!" );
-    }
-}
